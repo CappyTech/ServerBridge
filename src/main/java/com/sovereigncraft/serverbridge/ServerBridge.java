e0a19c1d39b2b21b69b7b0e26d97c9deda6749ab
CappyTech <133281784+CappyTech@users.noreply.github.com>
2025-05-18 02:08:55 +0100

    Update ServerBridge.java

--- src/main/java/com/sovereigncraft/serverbridge/ServerBridge.java
+++ src/main/java/com/sovereigncraft/serverbridge/ServerBridge.java
@@ -1,6 +1,8 @@
 package com.sovereigncraft.serverbridge;
 
 import com.sovereigncraft.matrix.MatrixClient;
+import com.sovereigncraft.matrix.MatrixUserTracker;
+
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.EventHandler;
@@ -9,14 +11,22 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 public class ServerBridge extends JavaPlugin implements Listener {
 
     private MatrixClient matrixClient;
+    private MatrixUserTracker userTracker;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         getServer().getPluginManager().registerEvents(this, this);
 
+        String homeserver = getConfig().getString("matrix.homeserver");
+        String adminToken = getConfig().getString("matrix.admin_token");
+
+        userTracker = new MatrixUserTracker(getDataFolder());
+
         matrixClient = new MatrixClient(getConfig().getConfigurationSection("matrix"), this.getLogger(), this);
         matrixClient.startPollingChatToMinecraft();
+
+        getCommand("chat").setExecutor(new ChatCommandExecutor(this, homeserver, adminToken));
         getLogger().info("ServerBridge enabled!");
     }
 
@@ -32,5 +42,9 @@ public class ServerBridge extends JavaPlugin implements Listener {
         String formatted = String.format("%s: %s", player, message);
 
         matrixClient.sendMessage(formatted); // This sends chat to the Matrix room
+    }
+
+    public MatrixUserTracker getUserTracker() {
+        return userTracker;
     }
 }
